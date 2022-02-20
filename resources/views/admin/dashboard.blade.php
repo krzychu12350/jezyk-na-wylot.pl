0dcf9309149b436276acecf5063e020fd88a9c12
Krzysztof Karaś <krzychu12350@interia.pl>
2022-02-20 15:38:40 +0100

    before adding Vue.js and Bootstrap

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Język na wylot</title>
+</head>
+<body>
+    <h1>Panel admina</h1>
+</body>
+</html>
+
---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Język na wylot</title>
+    <style>
+        .logo{
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="logo">
+        <img src="{{ asset('storage/images/logo.png') }}" alt="logo" title="">
+    </div>
+</body>
+</html>
